912c3969902f3fddd1b23667cc47b524da9c5df1
Kwonkyu <park2348190@naver.com>
2021-03-08 18:01:53 +0900

    Initial commit

---
+++ src/main/java/com/haruhiism/bbs/SimpleBbsApplication.java
@@ -0,0 +1,13 @@
+package com.haruhiism.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBbsApplication.class, args);
+	}
+
+}
