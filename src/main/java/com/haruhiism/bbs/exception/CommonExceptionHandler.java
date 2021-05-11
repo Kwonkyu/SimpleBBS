275f97f9171f7dfabf3ba75ce5b701205db22ab0
Kwonkyu <park2348190@naver.com>
2021-05-11 18:34:28 +0900

    Implemented file exception handlers
    # Purpose
    - 파일 업로드 시 발생할 수 있는 대표적인 두 가지 예외(파일 크기 제한 초과, 요청 크기 제한 초과) 처리 함수 구현.
    - 예외가 발생하며 파일 업로드가 취소됐을 때 요청 크기 제한을 초과하면 계속 500 에러로 넘어갔는데 톰캣 설정에서 swallow size를 설정하여 해결.
    - swallow size는 부적절한 파일 업로드가 발생했을 때 톰캣 서버에서 클라이언트 측 요청을 몇 바이트나 갖고있을지 설정하는듯.

--- src/main/java/com/haruhiism/bbs/exception/CommonExceptionHandler.java
+++ src/main/java/com/haruhiism/bbs/exception/CommonExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.multipart.MultipartException;
 import org.thymeleaf.exceptions.TemplateInputException;
 
 @ControllerAdvice
@@ -24,7 +26,7 @@ public class CommonExceptionHandler {
 
     @ExceptionHandler(AuthenticationFailedException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public String articleEditAuthFailed(Model model, AuthenticationFailedException exception) {
+    public String authenticationFailed(Model model, AuthenticationFailedException exception) {
         model.addAttribute("errorTitle", exception.errorTitle);
         model.addAttribute("errorDescription", exception.errorDescription);
         return "error/request-failed";
@@ -35,6 +37,22 @@ public class CommonExceptionHandler {
     public String templateInput(Model model, TemplateInputException exception) {
         model.addAttribute("errorTitle", "Thymeleaf template has not found.");
         model.addAttribute("errorDescription", exception.getLocalizedMessage());
+        return "error/request-failed";
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
+    public String requestSizeLimitExceeded(Model model) {
+        model.addAttribute("errorTitle", "Request Size Exceeds Limit.");
+        model.addAttribute("errorDescription", "Sent request's size exceeds limit. Please decrease request size.");
+        return "error/request-failed";
+    }
+
+    @ExceptionHandler(MultipartException.class)
+    @ResponseStatus(HttpStatus.PAYLOAD_TOO_LARGE)
+    public String fileSizeLimitExceeded(Model model) {
+        model.addAttribute("errorTitle", "Uploaded File Size Exceeds Limit.");
+        model.addAttribute("errorDescription", "Uploaded file's size exceeds limit. Please attach only files under limit.");
         return "error/request-failed";
     }
 }
